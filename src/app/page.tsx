512a12ddefd8a63ee29b6443fd2a548a5461df68
Mona Weichelt <morgana.weichelt@gmail.com>
2025-01-23 10:48:15 +0100

    basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,99 +2,45 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="h-screen flex flex-col">
+      <header className="min-h-32 md:min-h-48 flex justify-center items-center pb-4 md:pb-8 text-4xl md:text-6xl bg-primary text-white font-bold">
+        Mona Weichelt
+      </header>
+      <main className="flex-1 flex flex-col md:items-center">
+        <div className="sm:w-[600px] lg:w-[880px] relative -top-8 mx-8 flex flex-row rounded-lg shadow-md bg-white">
+          <Image
+            width={855}
+            height={1140}
+            src="/portrait.jpg"
+            alt="Portrait of the author"
+            className="hidden md:block md:w-64 lg:w-80 h-fit rounded-l-lg"
+          />
+          <Image
+            width={512}
+            height={512}
+            src="/portrait-cropped.jpg"
+            alt="Portrait of the author"
+            className="md:hidden w-24 h-fit rounded-l-lg"
+          />
+          <div className="p-1 md:px-4 md:pb-2 flex flex-col">
+            <h1 className="hidden md:block">Frontend Developer</h1>
+            <p className="hidden md:block">
+              Creating experiences for people is the core of my professional and
+              private life. I like to work where technology and the user meet,
+              focusing on the user experience and bringing technology to
+              practical application.
+            </p>
+            <p className="md:hidden">Passionate about user experience.</p>
+            <p className="md:mt-4 flex-1 hidden md:flex flex-col justify-end">
+              <span className="font-bold">Favourite technologies</span> React
+              (and Native), Typescript, Tailwind CSS
+            </p>
+          </div>
         </div>
+        <p className="md:hidden">Here is my mobile text</p>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="min-h-40 flex justify-center items-center bg-secondary">
+        Copyright © 2025 Mona Weichelt
       </footer>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#D30C7B",
+        secondary: "#FFCAD4",
       },
     },
   },
